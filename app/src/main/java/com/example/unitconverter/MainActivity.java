6b0cd0e6f4d5085a4bf8c6870689d64048e5471a
StarNikhil <nikhilbansode0508@gmail.com>
2022-10-20 22:13:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.unitconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private EditText editText;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (editText.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please Enter the value", Toast.LENGTH_SHORT).show();
+                } else {
+                    String s = editText.getText().toString();
+                    int kg = Integer.parseInt(s);
+                    double pound = 2.20462 * kg;
+                    textView.setText("The Pound Value is" + pound);
+
+                }
+            }
+        });
+    }
+}
